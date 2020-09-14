33e029d00d288e3807a708f64080e832a4631e03
SantoshUpadhye <65425770+SantoshUpadhye@users.noreply.github.com>
2020-09-14 22:11:31 +0200

    Add files via upload

--- src/visualization/visualize_V2.py
+++ src/visualization/visualize_V2.py
@@ -13,6 +13,12 @@ import os
 print(os.getcwd())
 df_input_large=pd.read_csv('C:/ProgramData/Anaconda3/eps_covid19/data/processed/COVID_final_set.csv',sep=';')
 
+fitted_final_data_dash_df = pd.read_csv('C:/ProgramData/Anaconda3/eps_covid19/data/processed/SIR/fitted_SIR_data.csv',sep=';')
+
+optimized_dash_df = pd.read_csv('C:/ProgramData/Anaconda3/eps_covid19/data/processed/SIR/optimized_SIR_data.csv',sep=';')
+
+ydata_dash_df = pd.read_csv('C:/ProgramData/Anaconda3/eps_covid19/data/processed/SIR/ydata_SIR_data.csv',sep=';')
+
 
 fig = go.Figure()
 
@@ -25,6 +31,8 @@ app.layout = html.Div([
     it covers the full walkthrough of: automated data gathering, data transformations,
     filtering and machine learning to approximating the doubling time, and
     (static) deployment of responsive dashboard.
+
+    * Extension of the existing model with SIR model was developed and incorporated in the same dashboard.
     '''),
 
     dcc.Markdown('''
@@ -39,7 +47,7 @@ app.layout = html.Div([
     ),
 
     dcc.Markdown('''
-        ## Select Timeline of confirmed COVID-19 cases or the approximated doubling time
+        ## Select Timeline of confirmed COVID-19 cases, confirmed cases COVID-19 filtered, approximated doubling time, doubling time filtered or SIR curve with filtered SIR curve
         '''),
 
 
@@ -50,6 +58,7 @@ app.layout = html.Div([
         {'label': 'Timeline Confirmed Filtered', 'value': 'confirmed_filtered'},
         {'label': 'Timeline Doubling Rate', 'value': 'confirmed_DR'},
         {'label': 'Timeline Doubling Rate Filtered', 'value': 'confirmed_filtered_DR'},
+        {'label': 'SIR curve and fitted SIR curve', 'value': 'SIR_value'},
     ],
     value='confirmed',
     multi=False
@@ -71,6 +80,10 @@ def update_figure(country_list,show_doubling):
         my_yaxis={'type':"log",
                'title':'Approximated doubling rate over 3 days (larger numbers are better #stayathome)'
               }
+    elif 'SIR' in show_doubling:
+        my_yaxis={'type': "log",
+               'title':'Population infected'
+              }
     else:
         my_yaxis={'type':"log",
                   'title':'Confirmed infected people (source johns hopkins csse, log-scale)'
@@ -82,14 +95,43 @@ def update_figure(country_list,show_doubling):
 
         df_plot=df_input_large[df_input_large['country']==each]
 
-        if show_doubling=='doubling_rate_filtered':
+        if show_doubling == 'SIR_value':
+            traces.append(dict(x=ydata_dash_df.date, #t, #df_plot.date,
+                                y=ydata_dash_df[each], #=df_plot[show_doubling],
+                                mode='markers+lines',
+                                opacity=0.9,
+                                line_width=2,
+                                marker_size=4,
+                                name=each
+
+                        )
+                )
+            traces.append(dict(x=ydata_dash_df.date, #df_plot.date,
+                                y=fitted_final_data_dash_df[each], #=df_plot[show_doubling],
+                                mode='markers+lines',
+                                opacity=0.9,
+                                line_width=2,
+                                marker_size=4,
+                                name=each+'_fitted'
+
+                        )
+                )
+
+
+        elif 'DR' in show_doubling: #elif show_doubling=='doubling_rate_filtered':
             df_plot=df_plot[['state','country','confirmed','confirmed_filtered','confirmed_DR','confirmed_filtered_DR','date']].groupby(['country','date']).agg(np.mean).reset_index()
+            traces.append(dict(x=df_plot.date,
+                                    y=df_plot[show_doubling],
+                                    mode='markers+lines',
+                                    opacity=0.9,
+                                    name=each
+                            )
+                    )
+
         else:
             df_plot=df_plot[['state','country','confirmed','confirmed_filtered','confirmed_DR','confirmed_filtered_DR','date']].groupby(['country','date']).agg(np.sum).reset_index()
        #print(show_doubling)
-
-
-        traces.append(dict(x=df_plot.date,
+            traces.append(dict(x=df_plot.date,
                                 y=df_plot[show_doubling],
                                 mode='markers+lines',
                                 opacity=0.9,
@@ -111,8 +153,10 @@ def update_figure(country_list,show_doubling):
 
                 yaxis=my_yaxis
         )
+            
     }
+
 
 if __name__ == '__main__':
 
-    app.run_server(debug=True, port=8051, use_reloader=False)
+    app.run_server(debug=True, use_reloader=False)
